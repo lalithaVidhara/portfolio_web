8ba382d091e5f4b282a1f030d10987443694487d
Lalitha Sri Vidhara <larlithasrividhara@gmail.com>
2025-01-17 02:30:19 +0530

    minor change

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -2,13 +2,22 @@
 
 import { PageInfo } from "../typings";
 
-export const fetchPageInfo = async() => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`);
-
+export const fetchPageInfo = async () => {
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+    if (!baseUrl) {
+      throw new Error("NEXT_PUBLIC_BASE_URL is not defined.");
+    }
+  
+    const res = await fetch(`${baseUrl}/api/getPageInfo`);
+    
+    // Ensure that the fetch is successful
+    if (!res.ok) {
+      throw new Error(`Failed to fetch page info: ${res.statusText}`);
+    }
+  
     const data = await res.json();
     const pageInfo: PageInfo = data.pageInfo;
-
-    // console.log('fetching', pageInfo);
-
+  
     return pageInfo;
-}
+  };
+  
